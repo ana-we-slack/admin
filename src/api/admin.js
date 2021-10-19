4f7130a6ee64d9030433b964e7b9c24699993933
Eslam Khalefa <eslamkhalefa19@gmail.com>
2021-10-19 13:58:55 +0200

    AA-3_create_admin_api_helper

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,12 +1,45 @@
 import axios from '../axios';
 
 // authorization
+
+axios.interceptors.response.use(
+  (res) => {
+    return res.data;
+  },
+  (error) => {
+    console.log(error.toJSON());
+  }
+);
+
+const createAdmin = (body) => {
+  return axios.post(`/admin`, { body });
+};
+
+const searchAdmin = (searchTerm, options) => {
+  return axios.get(`/admin`, { params: { searchTerm, options } });
+};
+const getAdmins = () => {
+  return axios.get(`/admin`);
+};
 const getAdminById = (id) => {
-  return axios.get(`/admin/${id}`).then((res) => res.data);
+  return axios.get(`/admin/${id}`);
+};
+
+const updateAdmin = (id, body) => {
+  return axios.patch(`/admin/${id}`, { body });
+};
+
+const deleteAdmin = (id) => {
+  return axios.delete(`/admin/${id}`);
 };
 
 const adminApi = {
   getAdminById,
+  deleteAdmin,
+  createAdmin,
+  updateAdmin,
+  getAdmins,
+  searchAdmin,
 };
 
 export default adminApi;
