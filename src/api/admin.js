bc9377e15340bc2e42d76212b4705e22a7d5e93b
Eslam Khalefa <eslamkhalefa19@gmail.com>
2021-10-19 11:23:36 +0200

    AA-3_create_admin_api_helper

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,12 +1,54 @@
 import axios from '../axios';
 
 // authorization
+
+const createAdmin = () => {
+  return axios
+    .post(`/admin`)
+    .then((res) => res.data)
+    .catch(function (error) {
+      console.log(error.toJSON());
+    });
+};
+
+const getAdmins = () => {
+  return axios
+    .get(`/admin`)
+    .then((res) => res.data)
+    .catch(function (error) {
+      console.log(error.toJSON());
+    });
+};
 const getAdminById = (id) => {
-  return axios.get(`/admin/${id}`).then((res) => res.data);
+  return axios
+    .get(`/admin/${id}`)
+    .then((res) => res.data)
+    .catch(function (error) {
+      console.log(error.toJSON());
+    });
+};
+
+const updateAdmin = (id) => {
+  return axios
+    .patch(`/admin/${id}`)
+    .then((res) => res.data)
+    .catch(function (error) {
+      console.log(error.toJSON());
+    });
+};
+
+const deleteAdmin = (id) => {
+  return axios.delete(`/admin/${id}`).catch(function (error) {
+    console.log(error.toJSON());
+  });
 };
 
 const adminApi = {
   getAdminById,
+  deleteAdmin,
+  createAdmin,
+  updateAdmin,
+  getAdmins,
 };
 
 export default adminApi;
