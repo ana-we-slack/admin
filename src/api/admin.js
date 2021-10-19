0d63a86aa2be1c347a96e7883c20be1707f7b0d2
Eslam Khalefa <eslamkhalefa19@gmail.com>
2021-10-19 13:08:33 +0200

    AA-3_create_admin_api_helper

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,12 +1,66 @@
 import axios from '../axios';
 
 // authorization
+
+const createAdmin = (first_name, last_name, email, password, username) => {
+  const adminData = { first_name, last_name, email, password, username };
+  return axios
+    .post(`/admin`, adminData)
+    .then((res) => res.data)
+    .catch(function (error) {
+      console.log(error.toJSON());
+    });
+};
+
+const searchAdmin = (search, filters, page_size) => {
+  const searchAdminFilters = { search, filters, page_size };
+  return axios
+    .get(`/admin`, { params: { searchAdminFilters } })
+    .then((res) => res.data)
+    .catch(function (error) {
+      console.log(error.toJSON());
+    });
+};
+const getAdmins = () => {
+  return axios
+    .get(`/admin`)
+    .then((res) => res.data)
+    .catch(function (error) {
+      console.log(error.toJSON());
+    });
+};
 const getAdminById = (id) => {
-  return axios.get(`/admin/${id}`).then((res) => res.data);
+  return axios
+    .get(`/admin/${id}`)
+    .then((res) => res.data)
+    .catch(function (error) {
+      console.log(error.toJSON());
+    });
+};
+
+const updateAdmin = (id, first_name, last_name, email, password, username) => {
+  const updatedAdminData = { first_name, last_name, email, password, username };
+  return axios
+    .patch(`/admin/${id}`, updatedAdminData)
+    .then((res) => res.data)
+    .catch(function (error) {
+      console.log(error.toJSON());
+    });
+};
+
+const deleteAdmin = (id) => {
+  return axios.delete(`/admin/${id}`).catch(function (error) {
+    console.log(error.toJSON());
+  });
 };
 
 const adminApi = {
   getAdminById,
+  deleteAdmin,
+  createAdmin,
+  updateAdmin,
+  getAdmins,
+  searchAdmin,
 };
 
 export default adminApi;
--- src/axios/index.js
+++ src/axios/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: { Authorization: 'Bearer my-token' },
 });
 
 export default instance;
